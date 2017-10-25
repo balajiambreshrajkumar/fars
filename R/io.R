23a95c5c1b06719d3f55e71680faa1b0f62d6065
Balaji Ambresh Rajkumar <balaji.ambresh@nym.hush.com>
2017-10-25 11:33:31 +0530

    Created documentation of functions.

---
+++ R/io.R
@@ -0,0 +1,100 @@
+library(dplyr)
+
+#' Create a dataframe table from the given csv filename.
+#' Should the filename not exist, then, an error is raised.
+#' @importMethodsFrom readr read_csv
+#' @importMethodsFrom  dplyr tbl_df
+#' @note No progress bar will be shown while reading the file.
+#' @param filename: The file to read from.
+#' @return dplyr tbl_df instance of the file content.
+#' @examples
+#' csv <- fars_read('avalid_filename.csv')
+#' > class(csv)
+#' [1] "tbl_df"     "tbl"        "data.frame"
+#' invalid <- fars_read('does_not_exist.csv')
+#' Error in fars_read("does_not_exist.csv") :
+#'  file 'does_not_exist.csv' does not exist
+fars_read <- function(filename) {
+  if(!file.exists(filename))
+    stop("file '", filename, "' does not exist")
+  data <- suppressMessages({
+    readr::read_csv(filename, progress = FALSE)
+  })
+  dplyr::tbl_df(data)
+}
+
+#' Create a file name for reading using \code{fars_read} method
+#' @param year: Integer year.
+#' @return Formatted filename of the form \code{accident_<year>.csv.bz2}
+#' @examples
+#' > make_filename(2017)
+#' [1] "accident_2017.csv.bz2"
+make_filename <- function(year) {
+  year <- as.integer(year)
+  sprintf("accident_%d.csv.bz2", year)
+}
+
+#' Create a dataframe only for the specific years.
+#' Each year would have a filename of the form \code{accident_<year>.csv.bz2}
+#' @note This call assumes that the data files are
+#' directly accessible under the current directory. Use \code{getwd()} to confirm.
+#' @importMethodsFrom dplyr select
+#' @importMethodsFrom dplyr mutate
+#' @importMethodsFrom magrittr %>%
+#' @param years: A vector of integer years
+#' @return list of dplyr tbl_df instances each for the year. If data for the year
+#' specified cannot be found, a warning is printed and NULL is returned for that entry
+#' in the year.
+#' @examples
+#' data <- fars_read_years(c(2014, 2015, 2021))
+#' Warning message:
+#' In value[[3L]](cond) : invalid year: 2021
+#'
+#' > head(x=data[1])
+#' [[1]]
+#' A tibble: 30,056 x 2
+#' MONTH  year
+#' # <int> <dbl>
+#'  1     1  2014
+#' 2     1  2014
+#' 3     1  2014
+#' 4     1  2014
+#' 5     1  2014
+#' 6     1  2014
+#' 7     1  2014
+#' 8     1  2014
+#' 9     1  2014
+#' 10     1  2014
+#' # ... with 30,046 more rows
+#' > head(x=data[2])
+#' [[1]]
+#' # A tibble: 32,166 x 2
+#' MONTH  year
+#' <int> <dbl>
+#'   1     1  2015
+#' 2     1  2015
+#' 3     1  2015
+#' 4     1  2015
+#' 5     1  2015
+#' 6     1  2015
+#' 7     1  2015
+#' 8     1  2015
+#' 9     1  2015
+#' 10     1  2015
+#' # ... with 32,156 more rows
+#' > head(data[3])
+#' [[1]]
+#' NULL
+fars_read_years <- function(years) {
+  lapply(years, function(year) {
+    file <- make_filename(year)
+    tryCatch({
+      dat <- fars_read(file)
+      dplyr::mutate(dat, year = year) %>%
+        dplyr::select(MONTH, year)
+    }, error = function(e) {
+      warning("invalid year: ", year)
+      return(NULL)
+    })
+  })
+}
---
+++ R/stats.R
@@ -0,0 +1,81 @@
+library(dplyr)
+
+#' Creates a summary of number of fars records per month for each year.
+#' @importMethodsFrom  dplyr bind_rows
+#' @importMethodsFrom dplyr group_by
+#' @importMethodsFrom dplyr summarize
+#' @importMethodsFrom tidyr spread
+#' @importMethodsFrom magrittr %>%
+#' @param years: A vector of integer years
+#' @return A data frame of the counts of number of fars records per year.
+#' Months are numbered between 1 and 12 (both ends included) and the counts
+#' are present under the years column. Invalid year entries are left out of the summary.
+#' @examples
+#' > ret <- fars_summarize_years(c(2013, 2015, 2021))
+#' Warning message:
+#'   In value[[3L]](cond) : invalid year: 2021
+#' > ret
+#' # A tibble: 12 x 3
+#' MONTH `2013` `2015`
+#' * <int>  <int>  <int>
+#'   1     1   2230   2368
+#' 2     2   1952   1968
+#' 3     3   2356   2385
+#' 4     4   2300   2430
+#' 5     5   2532   2847
+#' 6     6   2692   2765
+#' 7     7   2660   2998
+#' 8     8   2899   3016
+#' 9     9   2741   2865
+#' 10    10   2768   3019
+#' 11    11   2615   2724
+#' 12    12   2457   2781
+fars_summarize_years <- function(years) {
+  dat_list <- fars_read_years(years)
+  dplyr::bind_rows(dat_list) %>%
+    dplyr::group_by(year, MONTH) %>%
+    dplyr::summarize(n = n()) %>%
+    tidyr::spread(year, n)
+}
+
+#' Shows a graphic of the points where accidents took place
+#' for the given state number for the given year.
+#' @importMethodsFrom dplyr filter
+#' @importMethodsFrom magrittr %>%
+#' @importMethodsFrom maps map
+#' @importFrom  graphics points
+#' @param state.num: Integer ID of the state.
+#' @param year: Integer year.
+#' @return NULL
+#' @note Rasies an error if the state number is absent in the
+#' year fars dataset or the fars data file for the year is absent.
+#' @examples
+#' fars_map_state(1, 2021)
+#' Error in fars_read(filename) :
+#'  file 'accident_2021.csv.bz2' does not exist
+#'
+#' fars_map_state(1000, 2013)
+#' Error in fars_map_state(1000, 2013) : invalid STATE number: 1000
+#'
+#' fars_map_state(1, 2013)
+#' <shows image>
+fars_map_state <- function(state.num, year) {
+  filename <- make_filename(year)
+  data <- fars_read(filename)
+  state.num <- as.integer(state.num)
+
+  if(!(state.num %in% unique(data$STATE)))
+    stop("invalid STATE number: ", state.num)
+  data.sub <- dplyr::filter(data, STATE == state.num)
+  if(nrow(data.sub) == 0L) {
+    message("no accidents to plot")
+    return(invisible(NULL))
+  }
+  is.na(data.sub$LONGITUD) <- data.sub$LONGITUD > 900
+  is.na(data.sub$LATITUDE) <- data.sub$LATITUDE > 90
+  with(data.sub, {
+    maps::map("state", ylim = range(LATITUDE, na.rm = TRUE),
+              xlim = range(LONGITUD, na.rm = TRUE))
+    graphics::points(LONGITUD, LATITUDE, pch = 46)
+  })
+}
